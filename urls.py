d8455b7bc0387f5e11d9b21a036d833064095d2d
Daniel <dan.t.ching@gmail.com>
2015-07-28 20:00:46 -0700

    nominally functional

--- urls.py
+++ urls.py
@@ -4,6 +4,8 @@ from complete import views
 
 urlpatterns = patterns('',
     url(r'^$', views.main_page, name = 'main'),
-    url(r'^/api/result$', views.api_result, name = 'api_result'),
-
+    #ew
+    url(r'^api/get_id/$', views.api_get_none, name = 'none'),
+    url(r'^api/get_id/(?P<game_id>.*)/$', views.api_get_id, name = 'api_get_id'),
+    url(r'^api/search/$', views.api_search, name = 'api_search'),
 )
--- views.py
+++ views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
+from django.core import serializers
 
+import json
+
+from submit.models import Game 
 
 # Create your views here.
 
@@ -10,5 +14,22 @@ def main_page(request):
     }
     return render(request, 'complete/main.html', context)
 
-def api_result(request):
-    return HttpResponse('nothing here now')
+def api_search(request):
+    search = request.GET.get('query') 
+    data = serializers.serialize('json', Game.objects.filter(name__icontains = search), fields=('name'))
+    #yes, I *am* decoding the freshly encoded json into another json
+    #yes, I *know* that would be unncessary if I actually learned serializers
+    datum = json.dumps({'suggestions':json.loads(data)})
+    return HttpResponse(datum)
+
+def api_get_id(request, game_id):
+    try:
+        data = get_object_or_404(Game.objects.filter(id = game_id))
+    except:
+        return HttpResponse('oops')
+    #laaaazy
+    formatted = '<b>' + data.name + '</b><br>' + data.developer.name + '<br>' + data.url + '<br>' + data.notes
+    return HttpResponse(formatted)
+
+def api_get_none(request):
+    return HttpResponse('')
